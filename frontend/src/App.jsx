6a6c163a7bf1fabfe92beff0cb0aed99574dc8b6
Your Name <you@example.com>
2025-05-22 09:48:30 +0530

    Additional folder structure nabvar and header changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
+import Home from './Pages/Home/Home'
+import Cart from './Pages/Cart/Cart'
+import PlaceOrder from './Pages/PlaceOrder/PlaceOrder'
+import { Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div className='App'>
+    <div className='app'>
       <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/placeorder' element={<PlaceOrder />} />
+      </Routes>
     </div>
   )
 }
---
+++ frontend/src/Components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+            <h2>Order your favorite food here.</h2>
+            <p>Choose from a diverse menu featuring a delecatable array of dishes crafted with the all offering unique nutritional profiles. The taste, texture, and smell of food can significantly impact our enjoyment and experience, influencing our preferences and culinary choices.</p>
+            <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
