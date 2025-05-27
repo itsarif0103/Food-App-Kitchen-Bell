33d15222d57f32327ade008aff205ea0a59ca3a6
Your Name <you@example.com>
2025-05-27 21:39:38 +0530

    Moving create admin panel changes.

--- Admin/src/Pages/Add/Add.jsx
+++ Admin/src/Pages/Add/Add.jsx
@@ -1,12 +1,114 @@
-import React from 'react'
-import './Add.css'
+import React, { useState } from "react";
+import "./Add.css";
+import { assets } from "../../assets/admin_assets/assets";
+import axios from "axios";
+import { toast } from "react-toastify";
 
-const Add = () => {
+const Add = ({url}) => {
+    const [image, setImage] = useState(false);
+    const [data, setData] = useState({
+        name: "",
+        description: "",
+        category: "Salad",
+        price: "",
+    });
+    const onChangeHandler = (e) =>{
+        const name = e.target.name;
+        const value = e.target.value;
+        setData((prev) => ({ ...prev, [name]: value }));
+    }
+
+    const onsubmitHandler = async (e) => {
+        e.preventDefault();
+        const formData = new FormData();
+        formData.append("image", image);
+        formData.append("name", data.name);
+        formData.append("description", data.description);
+        formData.append("category", data.category);
+        formData.append("price", Number(data.price));
+        const response = await axios.post(
+            `${url}/api/food/add`, formData);
+            if(response.status === 200) {
+                setData({
+                    name: "",
+                    description: "",
+                    category: "Salad",
+                    price: "",
+                });
+                setImage(false);
+                toast.success(response.data.message, {
+                    position: "top-right",
+                    autoClose: 2000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                });
+            }
+            else {
+                toast.error(response.data.message, {
+                    position: "top-right",
+                    autoClose: 2000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                });
+            }
+    }   
+
   return (
-    <div>
-      add
+    <div className="add">
+      <form className="flex-col" onSubmit={onsubmitHandler}>
+        <div className="add-image-upload flex-col">
+          <p>Upload Image</p>
+          <label htmlFor="image">
+            <img src={image ? URL.createObjectURL(image) : assets.upload_area} alt="" />
+          </label>
+          <input type="file" id="image" onChange={(e)=>setImage(e.target.files[0])} hidden required />
+        </div>
+        <div className="add-product-name flex-col">
+          <p>Product Name</p>
+          <input onChange={onChangeHandler} value={data.name} type="text" name="name" placeholder="Type here..." />{" "}
+        </div>
+        <div className="add-product-description flex-col">
+          <p>Product Description</p>
+          <textarea
+            name="description"
+            onChange={onChangeHandler}
+            value={data.description}
+            rows={6}
+            placeholder="Write product description here..."
+            required
+          ></textarea>
+        </div>
+        <div className="add-category-price">
+          <div className="add-category flex-col">
+            <p>Product Category</p>
+            <select onChange={onChangeHandler} name="category">
+              <option value="Salad">Salad</option>
+              <option value="Rolls">Rolls</option>
+              <option value="Dessert">Dessert</option>
+              <option value="Sandwich">Sandwich</option>
+              <option value="Cake">Cake</option>
+              <option value="Pure Veg">Pure Veg</option>
+              <option value="Pasta">Pasta</option>
+              <option value="Noodles">Noodles</option>
+            </select>
+          </div>
+          <div className="add-price flex-col">
+            <p>Product Price</p>
+            <input onChange={onChangeHandler} value={data.price} type="number" name="price" placeholder="$20" />
+          </div>
+        </div>
+        <button type="submit" className="add-button">
+          ADD
+        </button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Add
+export default Add;
--- Admin/src/Pages/List/List.jsx
+++ Admin/src/Pages/List/List.jsx
@@ -1,12 +1,74 @@
-import React from 'react'
-import './List.css'
+import React, { useEffect, useState } from "react";
+import "./List.css";
+import axios from "axios";
+import { toast } from "react-toastify";
 
-const List = () => {
-  return (
-    <div>
-      list
-    </div>
-  )
-}
+const List = ({url}) => {
+  const [list, setList] = useState([]);
+  const fetchList = async () => {
+    const response = await axios.get(`${url}/api/food/list`);
+    if (response.status === 200) {
+      setList(response.data.data);
+    } else {
+      toast.error(response.data.message, {
+        position: "top-right",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    }
+  };
+  useEffect(() => {
+    fetchList();
+  }, []);
 
-export default List
+  const deleteItem = async (id) => {
+    const response = await axios.post(`${url}/api/food/remove`, { id: id });
+    await fetchList();
+    if(response.status === 200) {
+      toast.success(response.data.message, {
+        position: "top-right",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    } else {
+      toast.error(response.data.message, {
+        position: "top-right",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    }
+  }
+  return <div className="list add flex-col">
+        <p>All foods list</p>
+        <div className="list-table">
+            <div className="list-table-format title">
+                <b>Image</b><b>Name</b><b>Category</b><b>Price</b><b>Action</b>
+            </div>
+            {list.map((item, index) => {
+                return(
+                    <div key={index} className="list-table-format">
+                        <img src={`${url}/images/` + item.image} alt="" />
+                        <p>{item.name}</p>
+                        <p>{item.category}</p>
+                        <p>${item.price}</p>
+                        <p className="cursor" onClick={()=>{deleteItem(item._id)}}>X</p>
+                    </div>
+                )
+            })}
+        </div>
+  </div>;
+};
+
+export default List;
